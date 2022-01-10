31a476a300852974599571db892be27edcff6716
hamitmizrak <hamitmizrak@gmail.com>
2022-01-10 11:51:53 +0300

    JSR için Javaee.8.0.1 Sartnameler

---
+++ src/main/java/com/hamitmizrak/informatin/Sartname_Spesification.java
@@ -0,0 +1,9 @@
+package com.hamitmizrak.informatin;
+
+public class Sartname_Spesification {
+    //JSR =Java Spesification Requests
+   //    JSR 330 (CDI)
+//    JSR 369 (Servlet)
+//    JSR 380 (Bean)
+
+}
